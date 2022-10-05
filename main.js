c6356b37fbc00d4870e82bf9403e973bf671e59c
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-10-05 22:20:25 +0800

    chore: version 1.0.0 init

---
+++ main.js
@@ -0,0 +1,13 @@
+import App from './App'
+import uView from "uview-ui";
+import store from './store'
+
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+	...App,
+	store
+})
+Vue.use(uView);
+app.$mount()
---
+++ pages/about/help.vue
@@ -0,0 +1,35 @@
+<template>
+	<web-view :src="url" :webview-styles="webviewStyles">
+	</web-view>
+</template>
+
+<script>
+	import {
+		help
+	} from '@/constant/about.js'
+	export default {
+		data() {
+			return {
+				webviewStyles: {
+					progress: {
+						color: '#00FF00'
+					}
+				},
+				url: help
+			}
+		},
+		onLoad() {
+			uni.setClipboardData({
+				data: this.url,
+				success() {
+					uni.showToast({
+						title: '链接已复制'
+					})
+				}
+			})
+		}
+	}
+</script>
+
+<style>
+</style>
