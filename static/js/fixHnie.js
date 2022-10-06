eb08fe7c0b269acaf158e4dec5fc3f5540b2b557
Eric Moore <EricGittaMoore@duck.com>
2022-10-07 01:02:12 +0800

    fix: 优化注入提示 feat: 移除wap端头部导航栏 fix: 同步导航栏标题

--- static/js/fixHnie.js
+++ static/js/fixHnie.js
@@ -1,13 +1,52 @@
+/**
+ * 修复点击错误，防止他们使用的mui误伤了我
+ */
 window.addEventListener('load', () => {
-	$('[href^="javascript:openWindow"]').each((idx, el) => {
-		const href = $(el).attr('href')
-		const regex = /\('(.*?)'/
-		try {
-			const res = regex.exec(href)
-			$(el).attr('href', res[1])
-		} catch (e) {
-			//TODO handle the exception
-		}
-	})
-
+	setTimeout(() => {
+		$('[href^="javascript:openWindow"]').each((idx, el) => {
+			const href = $(el).attr('href')
+			const regex = /\('(.*?)'/
+			try {
+				const res = regex.exec(href)
+				$(el).attr('href', res[1])
+			} catch (e) {
+				//TODO handle the exception
+			}
+		})
+	}, 2000)
 })
+
+try {
+	/**
+	 * 删除头部
+	 */
+	window.addEventListener('load', () => {
+		document.querySelector('header.mui-bar-nav').style.display = 'none'
+
+
+		const style = document.createElement('style')
+		style.textContent = `
+.mui-bar-nav~.mui-content {
+	padding-top: 0!important;
+}
+.mui-bar-nav~.mui-content .mui-scroll {
+    top: 0!important;
+}
+`
+		document.body.appendChild(style)
+
+
+		/**
+		 * 同步标题
+		 */
+		let title = document.querySelector('h1.mui-title').textContent
+		uni.postMessage({
+			data: {
+				action: 'updateTitle',
+				title
+			}
+		})
+	})
+} catch (e) {
+	console.warn(e);
+}
